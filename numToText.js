eb863b169ee9f7cb3d23a266f68923c8f2e30aad
n-white <neil.r.whitehead@gmail.com>
2016-05-11 22:11:35 +0100

    "numToText solved"

--- numToText.js
+++ numToText.js
@@ -13,6 +13,31 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 
 */
 
-var numToText = function(str) {
+var numToText = function(string) {
+
+var numbers = {
+	1: 'one',
+	2: 'two',
+	3: 'three',
+	4: 'four',
+	5: 'five',
+	6: 'six',
+	7: 'seven',
+	8: 'eight',
+	9: 'nine',
+}
+
+	var answer = [];
+	
+	if(string.length == 1) {
+		return string;
+	} else if(numbers.hasOwnProperty(string[0])) {
+		answer = answer.concat(numbers[string[0]], numToText(string.slice(1)))
+	} else {
+		answer = answer.concat(string[0], numToText(string.slice(1)))
+	}
+	
+	return answer.join("")
 
 };
+
